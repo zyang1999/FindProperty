6abd178637653807a758e4b767bef521259ace2b
ganhongxu0214 <you@example.com>
2021-01-22 20:52:57 +0800

    commit message

--- FindProperty/Controllers/HomeController.cs
+++ FindProperty/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace FindProperty.Controllers
 
         public IActionResult Privacy()
         {
-            return View()x;
+            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
