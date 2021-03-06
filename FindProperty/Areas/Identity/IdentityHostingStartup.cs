a0bc92f08950c2d752d45d6248c757fc4c7c4f29
YANG <atyzyang@gmail.com>
2021-03-06 18:05:11 +0800

    Login, Logout, Register and Manag Index is added

---
+++ FindProperty/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FindProperty.Areas.Identity.Data;
+using FindProperty.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FindProperty.Areas.Identity.IdentityHostingStartup))]
+namespace FindProperty.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<FindPropertyContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("FindPropertyContextConnection")));
+
+                services.AddDefaultIdentity<FindPropertyUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<FindPropertyContext>();
+            });
+        }
+    }
+}
--- FindProperty/Startup.cs
+++ FindProperty/Startup.cs
@@ -24,6 +24,7 @@ namespace FindProperty
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +44,7 @@ namespace FindProperty
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
@@ -51,6 +53,7 @@ namespace FindProperty
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
